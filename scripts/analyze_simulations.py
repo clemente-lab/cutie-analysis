7e14dee8244d58814b7868542b3d08d4ef007e3d
Kevin Bu <kbpi314@gmail.com>
2020-05-01 12:33:58 -0400

    debugging

--- scripts/analyze_simulations.py
+++ scripts/analyze_simulations.py
@@ -226,61 +226,48 @@ def analyze_simulations(fold_value, statistic, param, corr_compare, classes,
                 for cc in ['False']:
                     for c in classes.split(','):
                         for samp in n_samp.split(','):
-                            # try:
-                            # subset dataframe
-                            df = results_df[results_df['parameter'] == p]
-                            df = df[df['fold_value'] == fv]
-                            df = df[df['stat'].isin(stat)]
-                            df = df[df['cooksd'] == cc]
-                            df = df[df['class'] == c]
-                            df = df[df['sample_size'] == samp]
-                            print(df['stat'].nunique())
-                            df['Method'] = df.apply(lambda row: new_label(row),axis=1)
-                            df = df.drop(['stat'], axis=1)
+                            # try statement exists because not all files will have an entry
+                            try:
+                                # subset dataframe
+                                df = results_df[results_df['parameter'] == p]
+                                df = df[df['fold_value'] == fv]
+                                df = df[df['stat'].isin(stat)]
+                                df = df[df['cooksd'] == cc]
+                                df = df[df['class'] == c]
+                                df = df[df['sample_size'] == samp]
+                                df['Method'] = df.apply(lambda row: new_label(row),axis=1)
+                                df = df.drop(['stat'], axis=1)
 
-                            # set styles
-                            sns.set(font_scale=1.4)
-                            sns.set_style("ticks", {'font.family':'sans-serif','font.sans-serif':'Helvetica'})
+                                # set styles
+                                sns.set(font_scale=1.4)
+                                sns.set_style("ticks", {'font.family':'sans-serif','font.sans-serif':'Helvetica'})
 
-                            # blue, red
-                            colors = ['#4F81BD','#C0504D']
-                            stats = ['CUTIE, p < 0.05', 'CUTIE, p > 0.05']
+                                # blue, red
+                                colors = ['#4F81BD','#C0504D']
+                                stats = ['CUTIE, p < 0.05', 'CUTIE, p > 0.05']
 
-                            title = 'Power Curves for simulations of ' + c + '\n scatterplots using ' + stat[0].capitalize()
+                                title = 'Power Curves for simulations of ' + c + '\n scatterplots using ' + stat[0].capitalize()
 
-                            plt.figure(figsize=(6,6))
-                            print(stat)
-                            print(df['corr_strength'])
-                            print(df['indicator'])
-                            print(df['Method'])
-                            print(len(df['corr_strength']))
-                            print(len(df['indicator']))
-                            print(len(df['Method']))
-                            print(df['corr_strength'].nunique())
-                            print(df['indicator'].nunique())
-                            print(df['Method'].nunique())
-                            print(len(sns.color_palette(colors)))
-                            print(sns.color_palette(colors))
+                                plt.figure(figsize=(6,6))
+                                ax = sns.pointplot(x="corr_strength", y="indicator", hue='Method',data=df, ci=95,
+                                    palette=sns.color_palette(colors), hue_order=stats)#, legend=False)
+                                ax.set_title(title, fontsize=15)
+                                plt.setp(ax.collections, alpha=.3) #for the markers
+                                plt.setp(ax.lines, alpha=.3)
+                                plt.ylim(-0.2, 1.2)
 
-                            ax = sns.pointplot(x="corr_strength", y="indicator", hue='Method',data=df, ci=95,
-                                palette=sns.color_palette(colors), hue_order=stats)#, legend=False)
-                            ax.set_title(title, fontsize=15)
-                            plt.setp(ax.collections, alpha=.3) #for the markers
-                            plt.setp(ax.lines, alpha=.3)
-                            plt.ylim(-0.2, 1.2)
+                                ax.set_ylabel('Proportion of Correlations classified as True using CUTIE')
+                                ax.set_xlabel('Correlation Strength')
+                                ax.set_xticklabels(corr_ticks,rotation=0)
+                                ax.set_yticklabels(['',0,0.2,0.4,0.6,0.8,1])
 
-                            ax.set_ylabel('Proportion of Correlations classified as True using CUTIE')
-                            ax.set_xlabel('Correlation Strength')
-                            ax.set_xticklabels(corr_ticks,rotation=0)
-                            ax.set_yticklabels(['',0,0.2,0.4,0.6,0.8,1])
-
-                            plt.tick_params(axis='both', which='both', top=False, right=False)
-                            sns.despine()
-                            plt.tight_layout()
-                            plt.savefig(output_dir + '_'.join([p, fv, stat[0], cc, c, samp]) + '.pdf')
-                            plt.close()
-                            # except:
-                            #     print('_'.join([p, fv, stat[0], cc, c, samp]))
+                                plt.tick_params(axis='both', which='both', top=False, right=False)
+                                sns.despine()
+                                plt.tight_layout()
+                                plt.savefig(output_dir + '_'.join([p, fv, stat[0], cc, c, samp]) + '.pdf')
+                                plt.close()
+                            except:
+                                 print('_'.join([p, fv, stat[0], cc, c, samp]))
 
 
     # cook D comparison
