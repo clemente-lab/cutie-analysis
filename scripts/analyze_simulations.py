0960c37aa38f0a558c8b401a620e4732a35d439f
Kevin Bu <kbpi314@gmail.com>
2020-05-01 11:40:00 -0400

    added parsing option for prebuilt df

--- scripts/analyze_simulations.py
+++ scripts/analyze_simulations.py
@@ -44,140 +44,148 @@ def analyze_simulations(fold_value, statistic, param, corr_compare, classes,
     Script for analysis of simulated data by CUTIE
     '''
 
-    def parse_log(f, cookd):
-        lines = [l.strip() for l in f.readlines()]
-        if cookd == 'True':
-            for l in lines:
-                if "number of correlations" in l:
-                    n_corr = int(l.split(' ')[-1])
-                elif "initial_corr" in l:
-                    initial_corr = int(l.split(' ')[-1])
-                elif "false correlations according to cookd" in l:
-                    false_corr = int(l.split(' ')[-1])
-                elif "true correlations according to cookd" in l:
-                    true_corr = int(l.split(' ')[-1])
-                elif "runtime" in l:
-                    runtime = float(l.split(' ')[-1])
-            rs_false = np.nan
-            rs_true = np.nan
+    # check if results df exists already
+    if not os.path.exists(output_dir + 'sim_results_df.txt'):
 
-        else:
-            # check if FDR correction defaulted
-            for l in lines:
-                if "number of correlations" in l:
-                    n_corr = int(l.split(' ')[-1])
-                elif "initial_corr" in l:
-                    initial_corr = int(l.split(' ')[-1])
-                elif "false correlations" in l:
-                    false_corr = int(l.split(' ')[-1])
-                elif "true correlations" in l:
-                    true_corr = int(l.split(' ')[-1])
-                elif "FP/TN1" in l:
-                    rs_false = int(l.split(' ')[-1])
-                elif "TP/FN1" in l:
-                    rs_true = int(l.split(' ')[-1])
-                elif "runtime" in l:
-                    runtime = float(l.split(' ')[-1])
+        def parse_log(f, cookd):
+            lines = [l.strip() for l in f.readlines()]
+            if cookd == 'True':
+                for l in lines:
+                    if "number of correlations" in l:
+                        n_corr = int(l.split(' ')[-1])
+                    elif "initial_corr" in l:
+                        initial_corr = int(l.split(' ')[-1])
+                    elif "false correlations according to cookd" in l:
+                        false_corr = int(l.split(' ')[-1])
+                    elif "true correlations according to cookd" in l:
+                        true_corr = int(l.split(' ')[-1])
+                    elif "runtime" in l:
+                        runtime = float(l.split(' ')[-1])
+                rs_false = np.nan
+                rs_true = np.nan
 
-        return n_corr, initial_corr, false_corr, true_corr, rs_false, rs_true, runtime
+            else:
+                # check if FDR correction defaulted
+                for l in lines:
+                    if "number of correlations" in l:
+                        n_corr = int(l.split(' ')[-1])
+                    elif "initial_corr" in l:
+                        initial_corr = int(l.split(' ')[-1])
+                    elif "false correlations" in l:
+                        false_corr = int(l.split(' ')[-1])
+                    elif "true correlations" in l:
+                        true_corr = int(l.split(' ')[-1])
+                    elif "FP/TN1" in l:
+                        rs_false = int(l.split(' ')[-1])
+                    elif "TP/FN1" in l:
+                        rs_true = int(l.split(' ')[-1])
+                    elif "runtime" in l:
+                        runtime = float(l.split(' ')[-1])
 
-    start, stop, step = [float(x) for x in rangestr.split(',')]
-    df_dict = {}
-    for p in param.split(','):
-        df_dict[p] = {}
-        for fv in fold_value.split(','):
-            df_dict[p][fv] = {}
-            for stat in statistic.split(','):
-                df_dict[p][fv][stat] = {}
-                for cc in corr_compare.split(','):
-                    df_dict[p][fv][stat][cc] = {}
-                    for seed in [str(x) for x in range(int(n_seed))]:
-                        df_dict[p][fv][stat][cc][seed] = {}
-                        for c in classes.split(','):
-                            df_dict[p][fv][stat][cc][seed][c] = {}
-                            for samp in n_samp.split(','):
-                                df_dict[p][fv][stat][cc][seed][c][samp] = {}
-                                for cor in ['{0:g}'.format(float(str(x))) for x in np.arange(start, stop+step, step)]:
-                                    df_dict[p][fv][stat][cc][seed][c][samp][cor] = (np.nan, np.nan)
+            return n_corr, initial_corr, false_corr, true_corr, rs_false, rs_true, runtime
 
 
-    file_dirs = glob.glob(input_dir + '*')
-    missing = []
-    done = 0
-    failed = []
+        start, stop, step = [float(x) for x in rangestr.split(',')]
+        df_dict = {}
+        for p in param.split(','):
+            df_dict[p] = {}
+            for fv in fold_value.split(','):
+                df_dict[p][fv] = {}
+                for stat in statistic.split(','):
+                    df_dict[p][fv][stat] = {}
+                    for cc in corr_compare.split(','):
+                        df_dict[p][fv][stat][cc] = {}
+                        for seed in [str(x) for x in range(int(n_seed))]:
+                            df_dict[p][fv][stat][cc][seed] = {}
+                            for c in classes.split(','):
+                                df_dict[p][fv][stat][cc][seed][c] = {}
+                                for samp in n_samp.split(','):
+                                    df_dict[p][fv][stat][cc][seed][c][samp] = {}
+                                    for cor in ['{0:g}'.format(float(str(x))) for x in np.arange(start, stop+step, step)]:
+                                        df_dict[p][fv][stat][cc][seed][c][samp][cor] = (np.nan, np.nan)
 
-    # troubleshooting
-    for f in file_dirs:
-        subset_files = glob.glob(f + '/*.txt')
-        subset_files.sort()
-        try:
+
+        file_dirs = glob.glob(input_dir + '*')
+        missing = []
+        done = 0
+        failed = []
+
+        # troubleshooting
+        for f in file_dirs:
+            subset_files = glob.glob(f + '/*.txt')
+            subset_files.sort()
+            try:
+                # grab the most recent txt (log) file
+                fn = subset_files[-1]
+            except:
+                print(f)
+
+        for f in file_dirs:
+            subset_files = glob.glob(f + '/*.txt')
+            subset_files.sort()
             # grab the most recent txt (log) file
             fn = subset_files[-1]
-        except:
-            print(f)
+            with open(fn, 'r') as rf:
+                label = f.split('/')[-1]
+                try:
+                    p, fv, stat, cc, seed, c, samp, cor = label.split('_')
+                    n_corr, initial_corr, false_corr, true_corr, rs_false, rs_true, runtime = parse_log(rf, cookd=cc)
+                    df_dict[p][fv][stat][cc][seed][c][samp][cor] = (true_corr, initial_corr)
+                    done += 1
+                except:
+                    failed.append(label)
+                    print(label)
+            if not subset_files:
+                missing.append(f)
 
-    for f in file_dirs:
-        subset_files = glob.glob(f + '/*.txt')
-        subset_files.sort()
-        # grab the most recent txt (log) file
-        fn = subset_files[-1]
-        with open(fn, 'r') as rf:
-            label = f.split('/')[-1]
-            try:
-                p, fv, stat, cc, seed, c, samp, cor = label.split('_')
-                n_corr, initial_corr, false_corr, true_corr, rs_false, rs_true, runtime = parse_log(rf, cookd=cc)
-                df_dict[p][fv][stat][cc][seed][c][samp][cor] = (true_corr, initial_corr)
-                done += 1
-            except:
-                failed.append(label)
-                print(label)
-        if not subset_files:
-            missing.append(f)
+        missing.sort()
+        # print([os.path.basename(x) for x in missing])
+        analysis_ids = []
+        ps = []
+        fvs = []
+        stats = []
+        ccs = []
+        seeds = []
+        class_labs = []
+        nsamps = []
+        cors = []
+        results = []
+        for p in param.split(','):
+            for fv in fold_value.split(','):
+                for stat in statistic.split(','):
+                    for cc in corr_compare.split(','):
+                        for seed in [str(x) for x in range(int(n_seed))]:
+                            for c in classes.split(','):
+                                for samp in n_samp.split(','):
+                                    for cor in ['{0:g}'.format(float(str(x))) for x in np.arange(start, stop+step, step)]:
+                                        d = df_dict[p][fv][stat][cc][seed][c][samp][cor]
+                                        # d = true corr, initial corr
+                                        # if initial corr is 0, we don't add it to df
+                                        if not np.isnan(d[0]):
+                                            if d[1] == 1:
+                                                analysis_ids.append('_'.join([p, fv, stat, cc, seed, c, samp, cor]))
+                                                ps.append(p)
+                                                fvs.append(fv)
+                                                stats.append(stat)
+                                                ccs.append(cc)
+                                                seeds.append(seed)
+                                                class_labs.append(c)
+                                                nsamps.append(samp)
+                                                cors.append(cor)
+                                                results.append(d[0])
 
-    missing.sort()
-    # print([os.path.basename(x) for x in missing])
-    analysis_ids = []
-    ps = []
-    fvs = []
-    stats = []
-    ccs = []
-    seeds = []
-    class_labs = []
-    nsamps = []
-    cors = []
-    results = []
-    for p in param.split(','):
-        for fv in fold_value.split(','):
-            for stat in statistic.split(','):
-                for cc in corr_compare.split(','):
-                    for seed in [str(x) for x in range(int(n_seed))]:
-                        for c in classes.split(','):
-                            for samp in n_samp.split(','):
-                                for cor in ['{0:g}'.format(float(str(x))) for x in np.arange(start, stop+step, step)]:
-                                    d = df_dict[p][fv][stat][cc][seed][c][samp][cor]
-                                    # d = true corr, initial corr
-                                    # if initial corr is 0, we don't add it to df
-                                    if not np.isnan(d[0]):
-                                        if d[1] == 1:
-                                            analysis_ids.append('_'.join([p, fv, stat, cc, seed, c, samp, cor]))
-                                            ps.append(p)
-                                            fvs.append(int(fv))
-                                            stats.append(stat)
-                                            ccs.append(cc)
-                                            seeds.append(seed)
-                                            class_labs.append(c)
-                                            nsamps.append(samp)
-                                            cors.append(cor)
-                                            results.append(d[0])
+        results_df = pd.DataFrame({'analysis_id': analysis_ids, 'parameter': ps, 'fold_value': fvs, 'stat': stats, 'cooksd': ccs,
+                                   'seed': seeds, 'class': class_labs,
+                                   'sample_size': nsamps, 'corr_strength': cors, 'indicator': results})
 
-    results_df = pd.DataFrame({'analysis_id': analysis_ids, 'parameter': ps, 'fold_value': fvs, 'stat': stats, 'cooksd': ccs,
-                               'seed': seeds, 'class': class_labs,
-                               'sample_size': nsamps, 'corr_strength': cors, 'indicator': results})
+        results_df.to_csv(output_dir + 'sim_results_df.txt', sep='\t')
 
-    # grab statistics
-    stat_pairs = []
-    for v, w in zip(statistic.split(',')[::2], statistic.split(',')[1::2]):
-        stat_pairs.append([v, w])
+    else:
+        # load in DF
+        results_df = pd.read_csv(output_dir + 'sim_results_df.txt', sep='\t')
+        # cast to string
+        for col in ['fold_value', 'cooksd', 'sample_size']:
+            sim_results_df[col] = sim_results_df[col].astype(str)
 
     # corr_ticks = [0, 0.05, 0.10, 0.15, 0.20, 0.25, 0.30, 0.35, 0.40, 0.45, \
     # 0.50, 0.55, 0.60, 0.65, 0.70, 0.75, 0.80, 0.85, 0.90, 0.95, 1]
@@ -204,6 +212,12 @@ def analyze_simulations(fold_value, statistic, param, corr_compare, classes,
             else:
                 # return row['stat'].capitalize() + ', CUTIE, p < 0.05'
                 return 'CUTIE, p < 0.05'
+
+
+    # grab statistics
+    stat_pairs = []
+    for v, w in zip(statistic.split(',')[::2], statistic.split(',')[1::2]):
+        stat_pairs.append([v, w])
 
     # indiv plots
     for p in param.split(','):
@@ -307,7 +321,6 @@ def analyze_simulations(fold_value, statistic, param, corr_compare, classes,
 
     print(len(missing),done,len(failed))
     print(results_df.head())
-    results_df.to_csv(output_dir + 'sim_results_df.txt', sep='\t')
 
 if __name__ == "__main__":
     analyze_simulations()
