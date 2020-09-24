542ff72c561a7709d2cac9787a423b55d017f925
Kevin Bu <kbpi314@gmail.com>
2020-09-24 14:32:18 -0400

    fixed double parsing error

--- scripts/analyze_real.py
+++ scripts/analyze_real.py
@@ -227,8 +227,8 @@ def analyze_real(fold_value, statistic, multi_corr, param, datasets,
                                 final_df = pd.concat(dfs, axis=0)
 
                                 # sort columns to remove duplicate var pairs
-                                final_df.sort_values(by=['var1', 'var2'])
-                                final_df.drop_duplicates(subset=['var1', 'var2'], keep='last')
+                                final_df = final_df.sort_values(by=['var1', 'var2'])
+                                final_df = final_df.drop_duplicates(subset=['var1', 'var2'], keep='last')
 
                                 n_corr = len(final_df)
                                 initial_df = final_df[final_df['class'].isin(['TP','FP','TN','FN'])]
