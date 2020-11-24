70bdfc7dbc19b77d81fe714c9597c740f0ef5d70
Kevin Bu <kbpi314@gmail.com>
2020-11-24 16:28:57 -0500

    debugging

--- scripts/analyze_simulations.py
+++ scripts/analyze_simulations.py
@@ -134,6 +134,7 @@ def analyze_simulations(fold_value, statistic, param, corr_compare, classes,
                     for x in corr_compare.split(','):
                         if x != 'False':
                             print(label)
+                            print(x)
                             n_corr, initial_corr, false_corr, true_corr, rs_false, rs_true, runtime = parse_log(rf, stat=x)
                             df_dict[p][fv][stat][x][seed][c][samp][cor] = (true_corr, initial_corr)
                 else: # cc is false, aka for CUTIE
